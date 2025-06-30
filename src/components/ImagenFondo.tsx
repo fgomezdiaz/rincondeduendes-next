9ca8f70d35a7880230ffcbecd6fd37a94bbdffc5
fgomezdiaz <pacoymari@gmail.com>
2025-06-30 08:59:10 +0200

    Varios

--- src/components/ImagenFondo.tsx
+++ src/components/ImagenFondo.tsx
@@ -3,7 +3,7 @@ import cover from "@/assets/images/Cover.png"
 
 export const ImagenFondo = () => {
   return (
-    <div className="relative w-full h-96 mt-2 md:mt-0">
+    <div className="relative w-full h-52 md:h-96 mt-2 md:mt-0">
     <Image
       src={cover}
       alt="Colorful abstract background"
@@ -11,13 +11,13 @@ export const ImagenFondo = () => {
       height={1200}
       className="object-cover w-full h-52 md:h-full"
     />
-    <div className="container mx-auto absolute inset-0 flex flex-col items-center max-w-4xl justify-center ">
-        <p className="text-base md:text-xl leading-relaxed text-black italic mt-8 max-w-72 md:max-w-none md:mt-0  md:mx-10">
+    <div className="container mx-auto absolute inset-0 flex flex-col items-center justify-center max-w-4xl h-full">
+        <p className="text-base md:text-xl leading-relaxed text-black italic md:mt-0 md:mx-10 text-center md:text-left px-4 w-full">
             Juguetería alternativa donde los juguetes artesanales de siempre conviven con las últimas
             innovaciones en artículos de madera responsable (eco-friendly).
         </p>
 
-        <p className="mt-4  md:m-6 mb-8 text-base text-black italic ">
+        <p className="mt-4 md:m-6 mb-8 text-base text-black italic text-center md:text-left px-4 w-full">
             Construimos fantasía con juguetes de verdad!
         </p>
     </div>
--- src/components/TabJuguetesSelect.tsx
+++ src/components/TabJuguetesSelect.tsx
@@ -1,14 +1,12 @@
+'use client'
 import { getAllMarcas } from '@/actions/marcas.actions'
 import { DrowpDownMarcas } from './drowndown/DropwDownMarcas'
 //import { DropwDownCategorias } from './drowndown/DropwDownCategorias'
 //import { getAllCategorias } from '@/actions/categorias.actions'
 //import { headers } from 'next/headers'
+import { useEffect, useState } from 'react';
 
 
-
-  const marcas = async() => {
-    return await getAllMarcas()
-  }
 
   // const categorias = async() => {
   //   return await getAllCategorias()
@@ -21,26 +19,31 @@ import { DrowpDownMarcas } from './drowndown/DropwDownMarcas'
 
 
 
-export const TabJuguetesSelect = async ({children1, children2}:Props) => {
+export const TabJuguetesSelect = ({children1, children2}:Props) => {
+  const [marcas, setMarcas] = useState<any[]>([]);
+  useEffect(() => {
+    getAllMarcas().then(setMarcas);
+  }, []);
+
   //const headersList = await headers()
 
   
   //console.log('headersList', headersList)
   
   return (
-    <nav className="flex mx-auto items-center fixed w-full z-20 mt-2 md:mt-0">
-    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto ">
+    <nav className="flex mx-auto items-center fixed w-full z-20 bg-gradient-to-r from-white/80 via-gray-200/80 to-white/80 backdrop-blur shadow-sm transition-all duration-300">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto ">
         <div className='flex flex-row items-center'>
           <div className="mb-2 mr-6 text-2xl italic">
             { children1}
           </div>
-          <DrowpDownMarcas marcas={await marcas()} />
+          <DrowpDownMarcas marcas={marcas} />
           {/* <DropwDownCategorias categorias={await categorias()}/> */}
           <div className="mb-2 ml-6 text-2xl italic">
             { children2}
           </div>
         </div>
-    </div>
+      </div>
     </nav>
   )
 }
